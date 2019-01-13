84bb5cf5818d0938528170439c102d3a2cc9f312
Alexander Korzun <korzun.sas@mail.ru>
2019-01-13 09:46:28 +0300

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -45,18 +45,44 @@ ostream& operator<<(ostream& s, const Student st)
 
 int main()
 {
-    vector<Student> v;
-
-    {
-        int g;
-        string s;
-        while (cin >> g >> s) {
-            v.push_back({g, s});
-        }
+    queue <int> q1, q2;
+    for (int i = 0; i < 5; ++i) {
+        int x;
+        cin >> x;
+        q1.push(x);
+    }
+    for (int i = 0; i < 5; ++i) {
+        int x;
+        cin >> x;
+        q2.push(x);
     }
 
-    stable_sort(v.begin(), v.end());
+    int turns = 0;
+    
+    while (true) {
+        int a1 = q1.front();
+        int a2 = q2.front();
+        q1.pop();
+        q2.pop();
 
-    output_separator = "\n";
-    cout << v << endl;
+        if (((a1 != 9 || a2 != 0) && a1 > a2) || (a1 == 0 && a2 == 9)) {
+            q1.push(a1);
+            q1.push(a2);
+        } else {
+            q2.push(a1);
+            q2.push(a2);
+        }
+        ++turns;
+
+        if (q1.empty()) {
+            cout << "second " << turns << endl;
+            return 0;
+        } else if (q2.empty()) {
+            cout << "first " << turns << endl;
+            return 0;
+        } else if (turns > 1000 * 1000) {
+            cout << "botva" << endl;
+            return 0;
+        }
+    }
 }
