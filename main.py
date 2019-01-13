aa6e4b71c90e52f8012bfef9882dd6eb9d61e772
Alexander Korzun <korzun.sas@mail.ru>
2019-01-13 11:39:24 +0300

    ok

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 #!/usr/bin/env python3
-print(bin(int(input()) - 1)[2:].replace('1', '5'))
+from functools import reduce
+factorial = lambda x: reduce(lambda a, b: a * b, range(2, x + 1), 1)
+print(factorial(int(input())))
