908cb7287031e4ec892c33b0f7d5b435f5b06d4d
Alexander Korzun <korzun.sas@mail.ru>
2019-01-13 11:45:38 +0300

    ok

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 #!/usr/bin/env python3
-from functools import reduce
-factorial = lambda x: reduce(lambda a, b: a * b, range(2, x + 1), 1)
-n, k = [int(input()) for i in [0]*2]
-print(factorial(n) // factorial(k) // factorial(n-k))
+n,p=map(int,input().split())
+print(pow(n,n,10**p))
